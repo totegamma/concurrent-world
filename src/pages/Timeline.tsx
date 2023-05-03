19b0b34055eac015bbad9c26efe7555f0b5dd72b
Naborisk <github@naborisk.com>
2023-05-03 17:04:24 +0900

    improve StreamsBar performace by separating component & change update logic

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -10,6 +10,7 @@ import ExploreIcon from '@mui/icons-material/Explore';
 import SearchIcon from '@mui/icons-material/Search';
 import SendIcon from '@mui/icons-material/Send';
 import { Draft } from '../components/Draft';
+import { StreamsBar } from '../components/StreamsBar';
 
 export interface TimelineProps {
     messages: IuseObjectList<StreamElement>;
@@ -28,42 +29,11 @@ export function Timeline(props: TimelineProps) {
     const theme = useTheme();
 
     return (<>
-        <Box sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            background:
-            theme.palette.primary.main,
-            padding: "5px",
-            borderRadius: "20px 20px 0 0"
-        }}>
-            <Paper
-                component="form"
-                elevation={0}
-                sx={{
-                    m: '3px 30px',
-                    p: '2px 4px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    width: '100%',
-                    height: '32px',
-                    borderRadius: '16px',
-                    background: lighten(theme.palette.primary.main, 0.3)
-                }}
-            >
-                <IconButton sx={{ p: '10px' }}>
-                    <ExploreIcon sx={{ color: "white" }} />
-                </IconButton>
-                <InputBase
-                    sx={{ ml: 1, flex: 1, color: '#fff' }}
-                    value={props.currentStreams}
-                    onChange={(e) => props.setCurrentStreams(e.target.value)}
-                />
-                <IconButton sx={{ p: '10px' }} onClick={_ => props.reload()}>
-                    <SearchIcon sx={{ color: "white" }} />
-                </IconButton>
-            </Paper>
-        </Box>
+        <StreamsBar 
+            currentStreams={props.currentStreams}
+            setCurrentStreams={props.setCurrentStreams}
+            reload={props.reload}
+        />
         <Box sx={{overflowY: "auto", padding: "20px"}}>
             <Box>
                 <Draft currentStreams={props.currentStreams} reload={props.reload} />
