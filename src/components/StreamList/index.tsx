cab3cabe510ab5500c187c2101e01327f5bd89a5
rassi0429 <taitokokoa+rassi@gmail.com>
2023-05-09 11:43:27 +0900

    wip

--- src/components/StreamList/index.tsx
+++ src/components/StreamList/index.tsx
@@ -1,85 +1,160 @@
+import React, { useState } from 'react'
 import {
-    Box,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemText
-} from '@mui/material'
-import { Link } from 'react-router-dom'
-import PercentIcon from '@mui/icons-material/Percent'
-import React, { useEffect, useContext, useMemo, useRef, useState } from 'react'
-import { ApplicationContext } from '../../App'
-import { type Stream } from '../../model'
-import { createPortal } from 'react-dom'
+    DndProvider,
+    getBackendOptions,
+    MultiBackend,
+    Tree
+} from '@minoru/react-dnd-treeview'
+import { CreateNewFolder } from '@mui/icons-material'
 
+import './index.css'
+import { Box, IconButton, Typography, useTheme } from '@mui/material'
+import type { ConcurrentTheme } from '../../model'
 interface Props {
     streams: string[]
 }
 
-export function StreamList(props: Props): JSX.Element {
-    const appData = useContext(ApplicationContext)
-    const [watchStreams, setWatchStreams] = useState<Stream[]>([])
+const initialData = [
+    {
+        id: 1,
+        parent: 0,
+        droppable: true,
+        text: 'Folder 1'
+    },
+    {
+        id: 2,
+        parent: 1,
+        text: 'File 1-1'
+    },
+    {
+        id: 3,
+        parent: 1,
+        text: 'File 1-2'
+    },
+    {
+        id: 4,
+        parent: 0,
+        droppable: true,
+        text: 'Folder 2'
+    },
+    {
+        id: 5,
+        parent: 4,
+        droppable: true,
+        text: 'Folder 2-1'
+    },
+    {
+        id: 6,
+        parent: 5,
+        text: 'File 2-1-1'
+    }
+]
 
-    useEffect(() => {
-        ;(async () => {
-            setWatchStreams(
-                (
-                    await Promise.all(
-                        props.streams.map(
-                            async (id) => await appData.streamDict?.get(id)
-                        )
-                    )
-                ).filter((e) => e) as Stream[]
-            )
-        })()
-    }, [props.streams])
+export function StreamList(props: Props): JSX.Element {
+    const theme = useTheme<ConcurrentTheme>()
+
+    const [treeData, setTreeData] = useState<any>(initialData)
+    const handleDrop = (newTreeData: any): void => {
+        setTreeData(newTreeData)
+    }
 
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '5px',
-                overflowY: 'auto',
-                overflowX: 'hidden'
-            }}
-        >
-            <List
-                dense
-                sx={{
-                    width: '100%',
-                    maxWidth: 360,
-                    display: 'flex',
-                    flexDirection: 'column'
-                }}
-            >
-                {watchStreams.map((stream) => {
-                    const labelId = `checkbox-list-secondary-label-${stream.id}`
-                    return (
-                        <ListItem key={stream.id} disablePadding>
-                            <ListItemButton
-                                component={Link}
-                                to={`/#${stream.id}`}
-                                sx={{ gap: 1 }}
-                            >
-                                <PercentIcon
-                                    sx={{
-                                        color: 'background.contrastText'
-                                    }}
-                                />
-                                <ListItemText
-                                    id={labelId}
-                                    primary={
-                                        stream.meta
-                                            ? JSON.parse(stream.meta).name
-                                            : 'backrooms'
-                                    }
-                                />
-                            </ListItemButton>
-                        </ListItem>
-                    )
-                })}
-            </List>
-        </Box>
+        <>
+            <Box display={'flex'} flexDirection={'row'}>
+                <Typography flex={1}>Streams</Typography>
+                <IconButton
+                    sx={{ color: 'background.contrastText' }}
+                    aria-label="add to shopping cart"
+                    className={'plus-icon'}
+                >
+                    <CreateNewFolder />
+                </IconButton>
+            </Box>
+            <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+                <Tree
+                    tree={treeData}
+                    rootId={0}
+                    onDrop={handleDrop}
+                    render={(node, { depth, isOpen, onToggle }) => (
+                        <div style={{ marginLeft: depth * 10 }}>
+                            {node.droppable && (
+                                <span onClick={onToggle}>
+                                    {isOpen ? '▼' : '▶︎'}
+                                </span>
+                            )}
+                            {node.text}
+                        </div>
+                    )}
+                />
+            </DndProvider>
+        </>
     )
 }
+
+// export function StreamList(props: Props): JSX.Element {
+//     const appData = useContext(ApplicationContext)
+//     const [watchStreams, setWatchStreams] = useState<Stream[]>([])
+//
+//     useEffect(() => {
+//         ;(async () => {
+//             setWatchStreams(
+//                 (
+//                     await Promise.all(
+//                         props.streams.map(
+//                             async (id) => await appData.streamDict?.get(id)
+//                         )
+//                     )
+//                 ).filter((e) => e) as Stream[]
+//             )
+//         })()
+//     }, [props.streams])
+//
+//     return (
+//         <Box
+//             sx={{
+//                 display: 'flex',
+//                 flexDirection: 'column',
+//                 gap: '5px',
+//                 overflowY: 'auto',
+//                 overflowX: 'hidden'
+//             }}
+//         >
+//             <List
+//                 dense
+//                 sx={{
+//                     width: '100%',
+//                     maxWidth: 360,
+//                     display: 'flex',
+//                     flexDirection: 'column'
+//                 }}
+//             >
+//                 {watchStreams.map((stream) => {
+//                     const labelId = `checkbox-list-secondary-label-${stream.id}`
+//                     return (
+//                         <ListItem key={stream.id} disablePadding>
+//                             <ListItemButton
+//                                 component={Link}
+//                                 to={`/#${stream.id}`}
+//                                 sx={{ gap: 1 }}
+//                             >
+//                                 <PercentIcon
+//                                     sx={{
+//                                         color: 'background.contrastText'
+//                                     }}
+//                                 />
+//                                 <ListItemText
+//                                     id={labelId}
+//                                     primary={
+//                                         stream.meta
+//                                             ? JSON.parse(stream.meta).name
+//                                             : 'backrooms'
+//                                     }
+//                                 />
+//                             </ListItemButton>
+//                         </ListItem>
+//                     )
+//                 })}
+//             </List>
+//         </Box>
+//     )
+// }
