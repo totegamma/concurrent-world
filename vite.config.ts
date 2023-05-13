2c11b8f84bc339d183ad540ac8a3acb3ddb34957
thotgamma <github@gammalab.net>
2023-05-14 00:26:00 +0900

    update manifest and icons

--- vite.config.ts
+++ vite.config.ts
@@ -26,31 +26,23 @@ export default defineConfig({
                 "short_name" : "Concurrent",
                 icons: [
                     {
-                        src: '/icon-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png',
+                        "src": "192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "maskable"
                     },
                     {
-                        src: '/icon-256x256.png',
-                        sizes: '256x256',
-                        type: 'image/png',
+                        "src": "512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "maskable"
                     },
                     {
-                        src: '/icon-384x384.png',
-                        sizes: '384x384',
-                        type: 'image/png',
-                    },
-                    {
-                        src: '/icon-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                    },
-                    {
-                        src: '/icon-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'any maskable',
-                    },
+                        "src": "splash.png",
+                        "sizes": "300x300",
+                        "type": "image/png",
+                        "purpose": "any"
+                    }
                 ]
             }
         }),
