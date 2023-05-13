8a985476b3370f78240e657aed5b91c28e811a35
thotgamma <github@gammalab.net>
2023-05-14 00:02:24 +0900

    add manifest

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,55 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), VitePWA({registerType: 'autoUpdate'}), BuildInfo()]
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: [
+                'offline.html',
+                'favicon.svg',
+                'favicon.ico',
+                'robots.txt',
+                'apple-touch-icon.png',
+            ],
+            manifest: {
+                "theme_color" : "#023059",
+                "background_color" : "#023059",
+                "display" : "standalone",
+                "scope" : "/",
+                "start_url" : "/",
+                "name" : "Concurrent",
+                "short_name" : "Concurrent",
+                icons: [
+                    {
+                        src: '/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icon-256x256.png',
+                        sizes: '256x256',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icon-384x384.png',
+                        sizes: '384x384',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ]
+            }
+        }),
+        BuildInfo()
+    ]
 })
