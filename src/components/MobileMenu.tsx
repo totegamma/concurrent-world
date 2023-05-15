541e9a068e1e4798731fa8561c87723397c1f444
thotgamma <github@gammalab.net>
2023-05-15 22:05:30 +0900

    fix navbar size

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -14,7 +14,7 @@ export function MobileMenu(): JSX.Element {
             <Box
                 sx={{
                     display: 'flex',
-                    height: 60,
+                    height: 49,
                     color: 'white',
                     justifyContent: 'space-around',
                     marginBottom: 'env(safe-area-inset-bottom)'
