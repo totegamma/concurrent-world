ed23b1587203c642c72ba5af103379a7bdf330f5
thotgamma <github@gammalab.net>
2023-05-16 23:01:34 +0900

    add: navigator settings layout

--- src/components/NavigatorSettings.tsx
+++ src/components/NavigatorSettings.tsx
@@ -1,3 +1,34 @@
+import { useState } from 'react'
+import { Divider, Tabs, Box, Tab, Typography } from '@mui/material'
+
 export function NavigatorSettings(): JSX.Element {
-    return <>testdayo</>
+    const [tab, setTab] = useState(0)
+
+    return (
+        <Box>
+            <Typography variant="h2">Navigator Settings</Typography>
+            <Divider />
+            <Tabs
+                value={tab}
+                onChange={(_, index) => {
+                    setTab(index)
+                }}
+            >
+                <Tab label="フォロー" />
+                <Tab label="投稿先" />
+            </Tabs>
+            {tab === 0 && (
+                <Box>
+                    <Typography variant="h3">ユーザー</Typography>
+                    <Typography variant="h3">ストリーム</Typography>
+                </Box>
+            )}
+            {tab === 1 && (
+                <Box>
+                    <Typography variant="h3">ホーム</Typography>
+                    <Typography variant="h3">ホーム以外</Typography>
+                </Box>
+            )}
+        </Box>
+    )
 }
