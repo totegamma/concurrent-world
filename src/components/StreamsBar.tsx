aa39dfa1f99ab2a0051ef91ff48214b71643a204
thotgamma <github@gammalab.net>
2023-05-17 10:41:08 +0900

    change: streamsbar now shows only current location

--- src/components/StreamsBar.tsx
+++ src/components/StreamsBar.tsx
@@ -1,24 +1,9 @@
 import { useContext, useEffect, useState } from 'react'
-import {
-    Paper,
-    IconButton,
-    InputBase,
-    Box,
-    useTheme,
-    Autocomplete,
-    Chip,
-    Modal
-} from '@mui/material'
+import { Paper, IconButton, Box, useTheme, Button } from '@mui/material'
 import ExploreIcon from '@mui/icons-material/Explore'
-import SearchIcon from '@mui/icons-material/Search'
-import {
-    Link,
-    useNavigate,
-    type Location as ReactLocation
-} from 'react-router-dom'
+import InfoIcon from '@mui/icons-material/Info'
+import { Link, type Location as ReactLocation } from 'react-router-dom'
 import { ApplicationContext } from '../App'
-import { type Stream } from '../model'
-import { NavigatorSettings } from './NavigatorSettings'
 
 export interface StreamsBarProps {
     location: ReactLocation
@@ -27,60 +12,31 @@ export interface StreamsBarProps {
 }
 
 export function StreamsBar(props: StreamsBarProps): JSX.Element {
-    const theme = useTheme()
-    const navigate = useNavigate()
-
-    const [allStreams, setAllStreams] = useState<Stream[]>([])
-    const [selectedStreams, setSelectedStreams] = useState<string[]>(
-        decodeURIComponent(props.location.hash.replace('#', '')).split(',')
-    )
-    const [streamMapper, setStreamMapper] = useState<Record<string, string>>({})
     const appData = useContext(ApplicationContext)
+    const theme = useTheme()
 
-    const [settingsOpen, setSettignsOpen] = useState<boolean>(false)
+    const [title, setTitle] = useState<string>('')
 
     useEffect(() => {
-        fetch(
-            appData.serverAddress +
-                'stream/list?schema=net.gammalab.concurrent.tbdStreamMeta'
-        ).then((data) => {
-            data.json().then((streams: Stream[]) => {
-                setAllStreams(streams)
-                setStreamMapper(
-                    Object.fromEntries(
-                        streams.map((stream) => [
-                            JSON.parse(stream.meta).name,
-                            stream.id
-                        ])
-                    )
-                )
-            })
-        })
-    }, [])
-
-    // force local streams to change in case of external input (i.e. sidebar button)
-    useEffect(() => {
-        ;(async () => {
-            setSelectedStreams(
-                (
-                    await Promise.all(
-                        props.location.hash
-                            .replace('#', '')
-                            .split(',')
-                            .map(
-                                async (id) =>
-                                    await appData.streamDict
-                                        .get(id)
-                                        .then((e) =>
-                                            e.meta
-                                                ? JSON.parse(e.meta).name
-                                                : null
-                                        )
-                            )
-                    )
-                ).filter((e) => e) as string[]
+        if (!props.location.hash || props.location.hash === '#') {
+            setTitle('Home')
+            return
+        }
+        Promise.all(
+            props.location.hash
+                .replace('#', '')
+                .split(',')
+                .map((e) => appData.streamDict.get(e))
+        ).then((a) => {
+            console.log(a)
+            setTitle(
+                a
+                    .map((e) => e.meta)
+                    .filter((e) => e)
+                    .map((e) => JSON.parse(e).name)
+                    .join(', ')
             )
-        })()
+        })
     }, [props.location.hash])
 
     return (
@@ -114,84 +70,26 @@ export function StreamsBar(props: StreamsBarProps): JSX.Element {
                     // submit logic (enter key) may be added here
                 }}
             >
-                <IconButton
-                    sx={{ p: '10px' }}
-                    onClick={() => {
-                        setSettignsOpen(true)
-                    }}
-                >
+                <IconButton sx={{ p: '10px' }} onClick={() => {}}>
                     <ExploreIcon sx={{ color: 'primary.contrastText' }} />
                 </IconButton>
-                <Autocomplete
-                    sx={{ width: 1 }}
-                    multiple
-                    value={selectedStreams[0] !== '' ? selectedStreams : []}
-                    options={allStreams.map(
-                        (stream) => JSON.parse(stream.meta).name
-                    )}
-                    onChange={(_, value) => {
-                        navigate(
-                            `/#${value.map((id) => streamMapper[id]).join(',')}`
-                        )
+                <Button
+                    sx={{
+                        width: 1,
+                        justifyContent: 'flex-start',
+                        color: 'primary.contrastText'
                     }}
-                    renderInput={(params) => {
-                        const { InputLabelProps, InputProps, ...rest } = params
-
-                        return (
-                            <InputBase
-                                {...params.InputProps}
-                                {...rest}
-                                sx={{ color: 'primary.contrastText' }}
-                                placeholder={
-                                    props.location.hash ? '' : 'following'
-                                }
-                            />
-                        )
-                    }}
-                    renderTags={(value, getTagProps) =>
-                        value.map((option, index) => (
-                            // disabling ESLint here becase 'key' should exist in {..getTagProps({index})}
-                            // eslint-disable-next-line
-                            <Chip
-                                label={option}
-                                sx={{ color: 'primary.contrastText' }}
-                                {...getTagProps({ index })}
-                            />
-                        ))
-                    }
-                />
+                >
+                    {title}
+                </Button>
                 <IconButton
                     sx={{ p: '10px' }}
                     component={Link}
-                    to={`/#${selectedStreams.join(',')}`}
+                    to={`/info/${props.location.hash}`}
                 >
-                    <SearchIcon sx={{ color: 'primary.contrastText' }} />
+                    <InfoIcon sx={{ color: 'primary.contrastText' }} />
                 </IconButton>
             </Paper>
-            <Modal
-                open={settingsOpen}
-                onClose={() => {
-                    setSettignsOpen(false)
-                }}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Paper
-                    sx={{
-                        position: 'absolute' as 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        width: 400,
-                        p: '20px'
-                    }}
-                >
-                    <NavigatorSettings
-                        followList={props.followList}
-                        setFollowList={props.setFollowList}
-                    />
-                </Paper>
-            </Modal>
         </Box>
     )
 }
