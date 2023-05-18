e0b09eb1152a03e1707c0ea91b1f58aec1aa58b0
rassi0429 <taitokokoa+rassi@gmail.com>
2023-05-19 01:04:11 +0900

    feat: ssl local dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import BuildInfo from 'vite-plugin-info'
 import { VitePWA } from 'vite-plugin-pwa'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -46,6 +47,7 @@ export default defineConfig({
                 ]
             }
         }),
-        BuildInfo()
+        BuildInfo(),
+        basicSsl()
     ]
 })
