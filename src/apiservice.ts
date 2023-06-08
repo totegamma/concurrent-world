bb0dad8d6d20a4a9fe98602392d45487eab7dd4d
totegamma <totegamma@WHITETT>
2023-06-08 20:39:20 +0900

    fix: jwt time restriction

--- src/apiservice.ts
+++ src/apiservice.ts
@@ -590,7 +590,7 @@ export default class ConcurrentApiClient {
             iss: this.userAddress,
             iat: Math.floor(new Date().getTime() / 1000).toString(),
             aud: this.host?.fqdn,
-            nbf: Math.floor(new Date().getTime() / 1000).toString(),
+            nbf: Math.floor((new Date().getTime() - 5 * 60 * 1000) / 1000).toString(),
             exp: Math.floor((new Date().getTime() + 5 * 60 * 1000) / 1000).toString(),
             ...claim
         })
