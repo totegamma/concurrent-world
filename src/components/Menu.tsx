724e88f2749d3600436b270fc1f22b9fc5798d2f
わおん <me@waon.me>
2023-07-03 17:24:07 +0900

    Update Menu.tsx and MobileMenu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,12 +2,14 @@ import {
     Box,
     ButtonBase,
     Divider,
+    IconButton,
     List,
     ListItem,
     ListItemButton,
     ListItemText,
     Typography,
-    useTheme
+    useTheme,
+    Tooltip
 } from '@mui/material'
 import { Link } from 'react-router-dom'
 
@@ -21,7 +23,6 @@ import NotificationsIcon from '@mui/icons-material/Notifications'
 import { memo, useContext } from 'react'
 import { ApplicationContext } from '../App'
 import type { ConcurrentTheme } from '../model'
-
 // @ts-expect-error vite dynamic import
 import buildTime from '~build/time'
 // @ts-expect-error vite dynamic import
@@ -138,28 +139,6 @@ export const Menu = memo<MenuProps>((props: MenuProps): JSX.Element => {
                                     </ListItemButton>
                                 </ListItem>
                                 <ListItem disablePadding>
-                                    <ListItemButton sx={{ gap: 1 }} component={Link} to="/identity">
-                                        <BadgeIcon
-                                            sx={{
-                                                color: 'background.contrastText'
-                                            }}
-                                        />
-
-                                        <ListItemText primary="Identity" />
-                                    </ListItemButton>
-                                </ListItem>
-                                <ListItem disablePadding>
-                                    <ListItemButton sx={{ gap: 1 }} component={Link} to="/settings">
-                                        <SettingsIcon
-                                            sx={{
-                                                color: 'background.contrastText'
-                                            }}
-                                        />
-
-                                        <ListItemText primary="Settings" />
-                                    </ListItemButton>
-                                </ListItem>
-                                <ListItem disablePadding>
                                     <ListItemButton sx={{ gap: 1 }} component={Link} to="/devtool">
                                         <BuildIcon
                                             sx={{
@@ -188,34 +167,72 @@ export const Menu = memo<MenuProps>((props: MenuProps): JSX.Element => {
                 >
                     <StreamList onClick={props.onClick} />
                 </Box>
-                <ButtonBase
-                    component={Link}
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'left',
-                        gap: 1
-                    }}
-                    to={'/entity/' + (appData.profile?.author ?? '')}
-                >
-                    <CCAvatar
-                        alt={appData.profile?.payload.body.username}
-                        avatarURL={appData.profile?.payload.body.avatar}
-                        identiconSource={api.userAddress}
+                <Box sx={{ p: 1 }}>
+                    <List
+                        onClick={props.onClick}
+                        disablePadding
                         sx={{
-                            width: '40px',
-                            height: '40px'
+                            display: 'flex',
+                            flexDirection: 'row',
+                            gap: 1,
+                            justifyContent: 'space-around',
+                            pb: 1
                         }}
-                    />
-                    <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center', flexFlow: 'column' }}>
-                        <Typography color={theme.palette.background.contrastText}>
-                            {appData.profile?.payload.body.username}
-                        </Typography>
-                        <Typography variant="caption" color={theme.palette.background.contrastText}>
-                            {api.host}
-                        </Typography>
-                    </Box>
-                </ButtonBase>
+                    >
+                        <ListItem disablePadding>
+                            <ListItemButton sx={{ gap: 1, padding: 0 }} component={Link} to="/identity">
+                                <Tooltip title="Identity">
+                                    <BadgeIcon
+                                        sx={{
+                                            color: 'background.contrastText'
+                                        }}
+                                    />
+                                </Tooltip>
+                                <ListItemText primary="Identity" primaryTypographyProps={{ fontSize: 13 }} />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton sx={{ gap: 1, padding: 0 }} component={Link} to="/settings">
+                                <Tooltip title="Settings">
+                                    <SettingsIcon
+                                        sx={{
+                                            color: 'background.contrastText'
+                                        }}
+                                    />
+                                </Tooltip>
+                                <ListItemText primary="Settings" primaryTypographyProps={{ fontSize: 13 }} />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                    <ButtonBase
+                        component={Link}
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'left',
+                            gap: 1
+                        }}
+                        to={'/entity/' + (appData.profile?.author ?? '')}
+                    >
+                        <CCAvatar
+                            alt={appData.profile?.payload.body.username}
+                            avatarURL={appData.profile?.payload.body.avatar}
+                            identiconSource={api.userAddress}
+                            sx={{
+                                width: '40px',
+                                height: '40px'
+                            }}
+                        />
+                        <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center', flexFlow: 'column' }}>
+                            <Typography color={theme.palette.background.contrastText}>
+                                {appData.profile?.payload.body.username}
+                            </Typography>
+                            <Typography variant="caption" color={theme.palette.background.contrastText}>
+                                {api.host}
+                            </Typography>
+                        </Box>
+                    </ButtonBase>
+                </Box>
             </Box>
         </Box>
     )
