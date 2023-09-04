dd6f94a8e8e9957e6522d056a94fb2ffc9af4ecf
totegamma <github@gammalab.net>
2023-09-04 20:50:48 +0900

    update ap ui

--- src/components/Activitypub/Setup.tsx
+++ src/components/Activitypub/Setup.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, TextField, Typography } from '@mui/material'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useApi } from '../../context/api'
 import { useSnackbar } from 'notistack'
 import { Schemas } from '@concurrent-world/client'
@@ -10,6 +10,38 @@ export const ApSetup = (): JSX.Element => {
     const pref = usePreference()
     const [userID, setUserID] = useState('')
     const { enqueueSnackbar } = useSnackbar()
+
+    const [loading, setLoading] = useState<boolean>(false)
+    const [entityFound, setEntityFound] = useState<boolean>(false)
+
+    useEffect(() => {
+        setLoading(true)
+        const timer = setTimeout(() => {
+            const requestOptions = {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json'
+                }
+            }
+            client.api
+                .fetchWithCredential(client.api.host, `/ap/api/person/${userID}`, requestOptions)
+                .then(async (res) => await res.json())
+                .then((profile) => {
+                    console.log('profile', profile)
+                    setEntityFound(true)
+                })
+                .catch((_e) => {
+                    setEntityFound(false)
+                })
+                .finally(() => {
+                    setLoading(false)
+                })
+        }, 300)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [userID])
 
     const register = async (): Promise<void> => {
         if (!client) {
@@ -84,6 +116,7 @@ export const ApSetup = (): JSX.Element => {
                 ActivityPubにおけるIDを設定します。
                 <br />
                 一度登録すると変更できません
+                <br />
             </Typography>
             <TextField
                 label="UserID"
@@ -91,14 +124,26 @@ export const ApSetup = (): JSX.Element => {
                 onChange={(x) => {
                     setUserID(x.target.value)
                 }}
+                error={userID.length > 0 && !userID.match(/^[a-zA-Z0-9_]+$/)}
+                helperText={
+                    userID.length > 0 && !userID.match(/^[a-zA-Z0-9_]+$/) ? 'a-z, A-Z, 0-9, _が使用できます' : ''
+                }
             />
+            {entityFound && (
+                <Typography>
+                    このユーザーは既に登録されています。
+                    <br />
+                    他のIDを試してください
+                </Typography>
+            )}
             <Button
                 variant="contained"
                 onClick={() => {
                     register()
                 }}
+                disabled={userID.length === 0 || !userID.match(/^[a-zA-Z0-9_]+$/) || entityFound || loading}
             >
-                Setup
+                {loading ? '確認中...' : '登録'}
             </Button>
         </Box>
     )
