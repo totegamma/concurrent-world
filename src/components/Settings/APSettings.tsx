4b06f2eef3cb9f0832f1cc4c24018429b5e7a155
totegamma <github@gammalab.net>
2023-10-28 18:56:37 +0900

    add activitypub inquery menu

--- src/components/Settings/APSettings.tsx
+++ src/components/Settings/APSettings.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, Button, Divider, IconButton, TextField, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { useApi } from '../../context/api'
 import { type ApEntity } from '../../model'
@@ -6,10 +6,16 @@ import { ApSetup } from '../Activitypub/Setup'
 import { ApProfileEditor } from '../Activitypub/ProfileEditor'
 import { ApFollowManager } from '../Activitypub/FollowManager'
 import { AddListButton } from '../AddListButton'
+import TravelExploreIcon from '@mui/icons-material/TravelExplore'
+import { CCDrawer } from '../ui/CCDrawer'
+import { useNavigate } from 'react-router-dom'
 
 export const APSettings = (): JSX.Element => {
     const client = useApi()
     const [entity, setEntity] = useState<ApEntity | null | undefined>(undefined)
+    const [openInquiry, setOpenInquiry] = useState(false)
+    const [url, setUrl] = useState('')
+    const navigate = useNavigate()
 
     useEffect(() => {
         const requestOptions = {
@@ -32,6 +38,20 @@ export const APSettings = (): JSX.Element => {
             })
     }, [])
 
+    const inquery = (url: string): void => {
+        client.api
+            .fetchWithCredential(client.api.host, `/ap/api/import?note=${encodeURIComponent(url)}`, {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
+            .then(async (res) => await res.json())
+            .then((data) => {
+                navigate(`/message/${data.content.id}@${data.content.author}`)
+            })
+    }
+
     if (entity === undefined) {
         return <>loading...</>
     }
@@ -49,12 +69,51 @@ export const APSettings = (): JSX.Element => {
             ) : (
                 <>
                     <Box display="flex" flexDirection="row" justifyContent="flex-end" gap={1} width="100%">
+                        <IconButton>
+                            <TravelExploreIcon
+                                onClick={() => {
+                                    setOpenInquiry(true)
+                                }}
+                            />
+                        </IconButton>
                         <AddListButton stream={entity.followstream} />
                     </Box>
                     <ApProfileEditor entity={entity} />
                     <ApFollowManager />
                 </>
             )}
+            <CCDrawer
+                open={openInquiry}
+                onClose={() => {
+                    setOpenInquiry(false)
+                }}
+            >
+                <Box display="flex" width="100%" gap={1} padding={1} flexDirection="column">
+                    <Typography variant="h2">照会</Typography>
+                    <Divider />
+                    <Box display="flex" width="100%" gap={1} padding={1}>
+                        <TextField
+                            label="照会"
+                            variant="outlined"
+                            value={url}
+                            onChange={(e) => {
+                                setUrl(e.target.value)
+                            }}
+                            sx={{
+                                flexGrow: 1
+                            }}
+                        />
+                        <Button
+                            variant="contained"
+                            onClick={() => {
+                                inquery(url)
+                            }}
+                        >
+                            照会
+                        </Button>
+                    </Box>
+                </Box>
+            </CCDrawer>
         </Box>
     )
 }
