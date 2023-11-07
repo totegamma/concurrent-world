aa2668ced50098b24250d55e705acaac7d504732
totegamma <github@gammalab.net>
2023-11-08 00:23:46 +0900

    fix emoji reaction profile override

--- src/components/Message/MessageReactions.tsx
+++ src/components/Message/MessageReactions.tsx
@@ -3,8 +3,15 @@ import { CCAvatar } from '../ui/CCAvatar'
 import { useMemo, useState } from 'react'
 import { Link as routerLink } from 'react-router-dom'
 
-import { Association, Message, ReplyMessageSchema, RerouteMessageSchema, Schemas, SimpleNoteSchema } from '@concurrent-world/client'
-import { EmojiAssociation } from '@concurrent-world/client/dist/types/schemas/emojiAssociation'
+import {
+    type Association,
+    type Message,
+    type ReplyMessageSchema,
+    type RerouteMessageSchema,
+    Schemas,
+    type SimpleNoteSchema
+} from '@concurrent-world/client'
+import { type EmojiAssociation } from '@concurrent-world/client/dist/types/schemas/emojiAssociation'
 
 export interface MessageReactionsProps {
     message: Message<SimpleNoteSchema | ReplyMessageSchema | RerouteMessageSchema>
@@ -13,15 +20,23 @@ export interface MessageReactionsProps {
 export const MessageReactions = (props: MessageReactionsProps): JSX.Element => {
     const theme = useTheme()
 
-    const [reactionMembers, setReactionMembers] = useState<Record<string, Association<EmojiAssociation>[]>>({})
+    const [reactionMembers, setReactionMembers] = useState<Record<string, Array<Association<EmojiAssociation>>>>({})
 
-    const ownReactions = useMemo(() => Object.fromEntries(props.message.ownAssociations.filter((association) => association.schema === Schemas.emojiAssociation).map((association) => [association.payload.body.imageUrl, association])), [props.message])
+    const ownReactions = useMemo(
+        () =>
+            Object.fromEntries(
+                props.message.ownAssociations
+                    .filter((association) => association.schema === Schemas.emojiAssociation)
+                    .map((association) => [association.payload.body.imageUrl, association])
+            ),
+        [props.message]
+    )
 
     if (!props.message.reactionCounts) {
         return <></>
     }
 
-    const loadReactionMembers = (reaction: string) => {
+    const loadReactionMembers = (reaction: string): void => {
         props.message.getReactions(reaction).then((reactions) => {
             setReactionMembers((prev) => {
                 return {
@@ -34,7 +49,7 @@ export const MessageReactions = (props: MessageReactionsProps): JSX.Element => {
 
     return (
         <Box display="flex" flexWrap="wrap" gap={1}>
-            {Object.entries(props.message.reactionCounts).map(([imageUrl, value]) => 
+            {Object.entries(props.message.reactionCounts).map(([imageUrl, value]) => (
                 <Tooltip
                     arrow
                     key={imageUrl}
@@ -42,53 +57,63 @@ export const MessageReactions = (props: MessageReactionsProps): JSX.Element => {
                         <Box display="flex" flexDirection="column" alignItems="right" gap={1}>
                             <Box display="flex" alignItems="center" gap={1}>
                                 <Box component="img" height="20px" src={imageUrl}></Box>
-                                {reactionMembers[imageUrl]?.[0].payload.body.shortcode ?? "Loading..."}
+                                {reactionMembers[imageUrl]?.[0].payload.body.shortcode ?? 'Loading...'}
                             </Box>
                             <Divider flexItem></Divider>
-                            {
-                                reactionMembers[imageUrl]?.map((reaction) => (
-                                    <Box
-                                        key={reaction.id}
+                            {reactionMembers[imageUrl]?.map((reaction) => (
+                                <Box
+                                    key={reaction.id}
+                                    sx={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        gap: 1,
+                                        textDecoration: 'none'
+                                    }}
+                                    component={routerLink}
+                                    to={reaction.payload.body.profileOverride?.link ?? '/entity/' + reaction.author}
+                                    target={reaction.payload.body.profileOverride?.link ? '_blank' : undefined}
+                                    rel={
+                                        reaction.payload.body.profileOverride?.link ? 'noopener noreferrer' : undefined
+                                    }
+                                >
+                                    <CCAvatar
+                                        avatarURL={
+                                            reaction.payload.body.profileOverride?.avatar ??
+                                            reaction.authorUser?.profile?.payload.body.avatar
+                                        }
+                                        identiconSource={reaction.author}
                                         sx={{
-                                            display: 'flex',
-                                            alignItems: 'center',
-                                            gap: 1,
-                                            textDecoration: 'none'
+                                            width: { xs: '12px', sm: '18px' },
+                                            height: { xs: '12px', sm: '18px' }
                                         }}
-                                        component={routerLink}
-                                        to={reaction.payload.body.profileOverride?.link ?? '/entity/' + reaction.author}
-                                        target={reaction.payload.body.profileOverride?.link ? '_blank' : undefined}
-                                        rel={reaction.payload.body.profileOverride?.link ? 'noopener noreferrer' : undefined}
+                                    />
+                                    <Typography
+                                        sx={{
+                                            fontSize: '0.8rem',
+                                            color: '#fff'
+                                        }}
                                     >
-                                        <CCAvatar
-                                            avatarURL={reaction.authorUser?.profile?.payload.body.avatar}
-                                            identiconSource={reaction.author}
-                                            sx={{
-                                                width: { xs: '12px', sm: '18px' },
-                                                height: { xs: '12px', sm: '18px' }
-                                            }}
-                                        />
-                                        <Typography
-                                            sx={{
-                                                fontSize: '0.8rem',
-                                                color: '#fff'
-                                            }}
-                                        >
-                                        {reaction.authorUser?.profile?.payload.body.username}</Typography>
-                                    </Box>
-                                ))
-                            }
+                                        {reaction.payload.body.profileOverride?.username ||
+                                            reaction.authorUser?.profile?.payload.body.username ||
+                                            'anonymous'}
+                                    </Typography>
+                                </Box>
+                            ))}
                         </Box>
                     }
                     placement="top"
-                    onOpen={() => loadReactionMembers(imageUrl)}
+                    onOpen={() => {
+                        loadReactionMembers(imageUrl)
+                    }}
                 >
                     <Button
                         sx={{
                             p: 0,
                             gap: 1,
                             display: 'flex',
-                            backgroundColor: ownReactions[imageUrl] ? alpha(theme.palette.primary.main, 0.5) : 'transparent',
+                            backgroundColor: ownReactions[imageUrl]
+                                ? alpha(theme.palette.primary.main, 0.5)
+                                : 'transparent',
                             borderColor: theme.palette.primary.main
                         }}
                         variant="outlined"
@@ -114,7 +139,7 @@ export const MessageReactions = (props: MessageReactionsProps): JSX.Element => {
                         </Typography>
                     </Button>
                 </Tooltip>
-            )}
+            ))}
         </Box>
     )
 }
