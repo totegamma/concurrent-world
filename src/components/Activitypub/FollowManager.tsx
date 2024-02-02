ae53d581f8b27a09717320b5e4d3c49da405b1eb
totegamma <github@gammalab.net>
2024-02-02 19:04:16 +0900

    update activitypub panel

--- src/components/Activitypub/FollowManager.tsx
+++ src/components/Activitypub/FollowManager.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { memo, useEffect, useState } from 'react'
 import { useApi } from '../../context/api'
 import { Avatar, Box, Button, Divider, IconButton, Link, Paper, TextField, Typography } from '@mui/material'
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1'
@@ -134,75 +134,83 @@ export const ApFollowManager = (): JSX.Element => {
     )
 }
 
-export const APUserCard = (props: { url: string; remove?: (_: string) => void }): JSX.Element => {
-    const [person, setPerson] = useState<any>(null)
-    const host = props.url.split('/')[2]
-    const shortID = `@${person?.preferredUsername}@${host}`
+export const APUserCard = memo<{ url: string; remove?: (_: string) => void }>(
+    (props: { url: string; remove?: (_: string) => void }): JSX.Element => {
+        const client = useApi()
+        const [person, setPerson] = useState<any>(null)
+        const host = props.url.split('/')[2]
+        const shortID = `@${person?.preferredUsername}@${host}`
 
-    useEffect(() => {
-        fetch(props.url, {
-            method: 'GET',
-            headers: {
-                accept: 'application/ld+json'
-            }
-        })
-            .then(async (res) => await res.json())
-            .then((data) => {
-                setPerson(data)
-            })
-    }, [props.url])
+        useEffect(() => {
+            client.api
+                .fetchWithCredential(client.api.host, `/ap/api/resolve/${encodeURI(props.url)}`, {
+                    method: 'GET',
+                    headers: {
+                        accept: 'application/ld+json'
+                    }
+                })
+                .then(async (res) => await res.json())
+                .then((data) => {
+                    setPerson(data.content)
+                })
+        }, [props.url])
 
-    if (!person) return <>loading...</>
+        console.log(person)
 
-    return (
-        <Paper
-            sx={{
-                display: 'flex',
-                p: 1,
-                backgroundImage: person.image ? `url(${person.image.url})` : undefined,
-                backgroundSize: 'cover',
-                gap: 1,
-                textDecoration: 'none'
-            }}
-        >
-            <Avatar src={person.icon?.url} />
-            <Box
+        if (!person) return <>loading...</>
+
+        return (
+            <Paper
                 sx={{
                     display: 'flex',
-                    backgroundColor: 'rgba(255,255,255,0.8)',
                     p: 1,
-                    borderRadius: 1,
-                    flexDirection: 'column',
-                    flex: 1
+                    backgroundImage: person.image ? `url(${person.image.url})` : undefined,
+                    backgroundSize: 'cover',
+                    gap: 1,
+                    textDecoration: 'none'
                 }}
             >
-                <Typography variant="h3">{person.name || person.preferredUsername}</Typography>
-                <Link underline="hover" href={props.url} target="_blank" rel="noopener noreferrer">
-                    @{person.preferredUsername}@{host}
-                </Link>
-            </Box>
-            {props.remove && (
+                <Avatar src={person.icon?.url} />
                 <Box
                     sx={{
                         display: 'flex',
+                        backgroundColor: 'rgba(255,255,255,0.8)',
+                        p: 1,
+                        borderRadius: 1,
                         flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center'
+                        flex: 1
                     }}
                 >
-                    <IconButton
-                        onClick={() => props.remove?.(shortID)}
+                    <Typography variant="h3">{person.name || person.preferredUsername}</Typography>
+                    <Link underline="hover" href={props.url} target="_blank" rel="noopener noreferrer">
+                        @{person.preferredUsername}@{host}
+                    </Link>
+                </Box>
+                {props.remove && (
+                    <Box
                         sx={{
-                            backgroundColor: 'rgba(255,255,255,0.8)',
-                            '&:hover': {
-                                backgroundColor: 'rgba(255,255,255,1)'
-                            }
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center'
                         }}
                     >
-                        <PersonRemoveIcon />
-                    </IconButton>
-                </Box>
-            )}
-        </Paper>
-    )
-}
+                        <IconButton
+                            onClick={() => props.remove?.(shortID)}
+                            sx={{
+                                backgroundColor: 'rgba(255,255,255,0.8)',
+                                '&:hover': {
+                                    backgroundColor: 'rgba(255,255,255,1)'
+                                }
+                            }}
+                        >
+                            <PersonRemoveIcon />
+                        </IconButton>
+                    </Box>
+                )}
+            </Paper>
+        )
+    }
+)
+
+APUserCard.displayName = 'APUserCard'
