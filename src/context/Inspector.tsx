d4bf0727e19d81e2edc9f9f3453b41322246d6c2
totegamma <github@gammalab.net>
2024-02-24 01:05:48 +0900

    update inspector

--- src/context/Inspector.tsx
+++ src/context/Inspector.tsx
@@ -6,6 +6,8 @@ import { validateSignature, type CoreMessage, type CoreAssociation } from '@conc
 import { Codeblock } from '../components/ui/Codeblock'
 import { MessageContainer } from '../components/Message/MessageContainer'
 import { CCDrawer } from '../components/ui/CCDrawer'
+import { type Key } from '@concurrent-world/client/dist/types/model/core'
+import { KeyCard } from '../components/ui/KeyCard'
 
 export interface InspectorState {
     inspectingItem: { messageId: string; author: string } | null
@@ -23,27 +25,95 @@ export const InspectorProvider = (props: InspectorProps): JSX.Element => {
     const [inspectingItem, inspectItem] = useState<{ messageId: string; author: string } | null>(null)
     const [message, setMessage] = useState<CoreMessage<any> | undefined>()
     const [associations, setAssociations] = useState<Array<CoreAssociation<any>>>([])
-    const [signatureIsValid, setSignatureIsValid] = useState<boolean>(false)
-    const [currentHost, setCurrentHost] = useState<string>('')
+    const [keyResolution, setKeyResolution] = useState<Key[]>([])
 
     useEffect(() => {
         if (!inspectingItem) return
 
-        client.api.getEntity(inspectingItem.author).then((entity) => {
-            if (!entity) return
-            setCurrentHost(entity.domain || client.api.host)
-        })
-
         client.api.getMessageWithAuthor(inspectingItem.messageId, inspectingItem.author).then((msg) => {
             if (!msg) return
-            setSignatureIsValid(validateSignature(msg.rawpayload, msg.signature, msg.author))
             setMessage(msg)
+            if (msg.payload.keyID && msg.payload.keyID !== '') {
+                client.api.getKeychain(msg.payload.keyID).then((keys) => {
+                    console.log('resolution:', keys)
+                    setKeyResolution(keys)
+                })
+            }
         })
 
         client.api.getMessageAssociationsByTarget(inspectingItem.messageId, inspectingItem.author).then((assocs) => {
             setAssociations(assocs)
         })
     }, [inspectingItem])
+
+    const signatureIsValid = useMemo(() => {
+        if (message) {
+            return validateSignature(
+                message.rawpayload,
+                message.signature,
+                message.payload.keyID ?? message.payload.signer
+            )
+        }
+        return false
+    }, [message])
+
+    const previewMessage = useMemo(() => {
+        const msg: any = message
+        if (msg) {
+            msg.associations = 'REDACTED'
+            msg.ownAssociations = 'REDACTED'
+        }
+        return msg
+    }, [message])
+
+    const isSignedBySubkey = useMemo(() => {
+        if (message) {
+            return message.payload.keyID && message.payload.keyID !== ''
+        }
+        return false
+    }, [message])
+
+    const isKeyResolutionOK = useMemo(() => {
+        const allKeyValid = keyResolution.every((key) => {
+            try {
+                return validateSignature(key.enactPayload, key.enactSignature, key.parent)
+            } catch (e) {
+                return false
+            }
+        })
+
+        if (!allKeyValid) return false
+
+        const allKeyNotRevoked = keyResolution.every((key) => {
+            console.log('checking key:', key)
+            if (key.revokePayload?.startsWith('{') && key.revokeSignature) {
+                try {
+                    const obj = JSON.parse(key.revokePayload)
+                    return !validateSignature(key.revokePayload, key.revokeSignature, obj.keyID ?? obj.signer)
+                } catch (e) {
+                    return true
+                }
+            } else {
+                console.log('none')
+            }
+            return true
+        })
+
+        if (!allKeyNotRevoked) return false
+
+        const rootkey = keyResolution[0]?.root
+        if (!rootkey) return true
+
+        let previousKey: string | null = null
+
+        for (let i = 1; i < keyResolution.length; i++) {
+            if (keyResolution[i].root !== rootkey) return false
+            if (previousKey && keyResolution[i].parent !== previousKey) return false
+            previousKey = keyResolution[i].id
+        }
+
+        return true
+    }, [keyResolution])
 
     return (
         <InspectorContext.Provider
@@ -73,58 +143,55 @@ export const InspectorProvider = (props: InspectorProps): JSX.Element => {
                         <Paper sx={{ m: '10px 0', p: '0 20px' }} elevation={0} variant="outlined">
                             <MessageContainer messageID={message.id} messageOwner={message.author} />
                         </Paper>
+                        <Typography variant="h2" sx={{ mt: 1 }}>
+                            CheckSig:
+                        </Typography>
+
                         {signatureIsValid ? (
-                            <Alert severity="success">Signature is valid!</Alert>
+                            isSignedBySubkey ? (
+                                <Alert severity="success">
+                                    Signature is valid!
+                                    <br />
+                                    With subkey:
+                                    <br />
+                                    {message.payload.keyID}
+                                </Alert>
+                            ) : (
+                                <Alert severity="success">Signature is valid!</Alert>
+                            )
                         ) : (
                             <Alert severity="error">Signature is invalid!</Alert>
                         )}
-                        <Table sx={{ fontSize: '10px' }}>
-                            <TableBody>
-                                <TableRow>
-                                    <TableCell>
-                                        <b>ID</b>
-                                    </TableCell>
-                                    <TableCell>{message.id}</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>
-                                        <b>Author</b>
-                                    </TableCell>
-                                    <TableCell>{message.author}</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>
-                                        <b>Host</b>
-                                    </TableCell>
-                                    <TableCell>{currentHost}</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>
-                                        <b>Schema</b>
-                                    </TableCell>
-                                    <TableCell>{message.schema}</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell sx={{ wordBreak: 'keep-all' }}>
-                                        <b>Signature</b>
-                                    </TableCell>
-                                    <TableCell>{message.signature}</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>
-                                        <b>Streams</b>
-                                    </TableCell>
-                                    <TableCell>{message.streams.join('\n')}</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>
-                                        <b>Created</b>
-                                    </TableCell>
-                                    <TableCell>{new Date(message.cdate).toLocaleString()}</TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                        <Typography variant="h2">Payload:</Typography>
+
+                        {isSignedBySubkey && (
+                            <>
+                                <Typography variant="h2" sx={{ mt: 1 }}>
+                                    CheckKeyResolution:
+                                </Typography>
+
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        gap: 1
+                                    }}
+                                >
+                                    {isKeyResolutionOK ? (
+                                        <Alert severity="success">Key resolution is valid!</Alert>
+                                    ) : (
+                                        <Alert severity="error">Key resolution is invalid!</Alert>
+                                    )}
+
+                                    {keyResolution.map((key) => (
+                                        <KeyCard short key={key.id} item={key} />
+                                    ))}
+                                </Box>
+                            </>
+                        )}
+
+                        <Typography variant="h2" sx={{ mt: 1 }}>
+                            Message:
+                        </Typography>
                         <Box
                             sx={{
                                 borderRadius: '10px',
@@ -132,10 +199,12 @@ export const InspectorProvider = (props: InspectorProps): JSX.Element => {
                             }}
                         >
                             <Codeblock language="json">
-                                {JSON.stringify(message.payload ?? 'null', null, 4)?.replaceAll('\\n', '\n')}
+                                {JSON.stringify(previewMessage ?? 'null', null, 4)?.replaceAll('\\n', '\n')}
                             </Codeblock>
                         </Box>
-                        <Typography variant="h2">Associations:</Typography>
+                        <Typography variant="h2" sx={{ mt: 1 }}>
+                            Associations:
+                        </Typography>
                         <Box
                             sx={{
                                 borderRadius: '10px',
