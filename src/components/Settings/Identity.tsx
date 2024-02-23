c1d623faf16dcf11e7406caa7149d1351c8d1531
totegamma <github@gammalab.net>
2024-02-23 19:00:53 +0900

    add key list

--- src/components/Settings/Identity.tsx
+++ src/components/Settings/Identity.tsx
@@ -1,13 +1,30 @@
-import { Alert, Box } from '@mui/material'
+import { Alert, Box, IconButton, Menu, MenuItem, Paper, Typography } from '@mui/material'
 import Tilt from 'react-parallax-tilt'
 import { Passport } from '../theming/Passport'
-import { Suspense, lazy } from 'react'
+import { Suspense, lazy, useEffect, useState } from 'react'
+import { useApi } from '../../context/api'
+import { type Key } from '@concurrent-world/client/dist/types/model/core'
+
+import KeyIcon from '@mui/icons-material/Key'
+import KeyOffIcon from '@mui/icons-material/KeyOff'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 
 const SwitchMasterToSub = lazy(() => import('../SwitchMasterToSub'))
 
 export const IdentitySettings = (): JSX.Element => {
+    const client = useApi()
     const mnemonic = JSON.parse(localStorage.getItem('Mnemonic') || 'null')
     const subkey = JSON.parse(localStorage.getItem('SubKey') || 'null')
+
+    const [keys, setKeys] = useState<Key[]>([])
+    const [target, setTarget] = useState<string | null>(null)
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+    useEffect(() => {
+        client.api.getKeyList().then((res) => {
+            setKeys(res)
+        })
+    }, [])
 
     return (
         <Box
@@ -58,6 +75,90 @@ export const IdentitySettings = (): JSX.Element => {
             )}
 
             {!subkey && !mnemonic && <Alert severity="error">特殊なログイン状態のようです。</Alert>}
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 1
+                }}
+            >
+                {keys.map((key) => (
+                    <Paper
+                        key={key.id}
+                        variant="outlined"
+                        sx={{
+                            padding: 1,
+                            display: 'flex',
+                            flexDirection: 'row',
+                            gap: 1,
+                            alignItems: 'center'
+                        }}
+                    >
+                        {key.revokePayload === 'null' ? (
+                            <KeyIcon
+                                sx={{
+                                    width: '40px',
+                                    height: '40px'
+                                }}
+                            />
+                        ) : (
+                            <KeyOffIcon
+                                sx={{
+                                    width: '40px',
+                                    height: '40px'
+                                }}
+                            />
+                        )}
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                gap: 1,
+                                flex: 1
+                            }}
+                        >
+                            <Typography>ID: {key.id}</Typography>
+                            <Typography>Created: {key.cdate}</Typography>
+                            <Typography>Parent: {key.parent}</Typography>
+                        </Box>
+                        <IconButton
+                            sx={{
+                                width: '40px',
+                                height: '40px'
+                            }}
+                            onClick={(event) => {
+                                setTarget(key.id)
+                                setAnchorEl(event.currentTarget)
+                            }}
+                        >
+                            <MoreHorizIcon />
+                        </IconButton>
+                    </Paper>
+                ))}
+                <Menu
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={() => {
+                        setAnchorEl(null)
+                    }}
+                >
+                    <MenuItem
+                        onClick={() => {
+                            if (target === null) {
+                                return
+                            }
+                            client.api.revokeSubkey(target).then(() => {
+                                client.api.getKeyList().then((res) => {
+                                    setKeys(res)
+                                })
+                            })
+                        }}
+                    >
+                        無効化
+                    </MenuItem>
+                </Menu>
+            </Box>
         </Box>
     )
 }
