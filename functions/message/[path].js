967e29f07c467a2af9d34bd323e0bcb2a1fa8c2e
Naborisk <github@naborisk.com>
2024-04-02 01:49:58 +0900

    test request

--- functions/message/[path].js
+++ functions/message/[path].js
@@ -2,5 +2,10 @@ export function onRequest(context) {
     const { path } = context.params
     const [messageId, ccid] = path.split('@')
 
-    return new Response(`Message ID: ${messageId}, CCID: ${ccid}`)
+    fetch(`https://hub.concurrent.world/api/v1/address/${ccid}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            return new Response(`Message ID: ${messageId}, CCID: ${ccid}, data: ${data}`)
+        })
 }
