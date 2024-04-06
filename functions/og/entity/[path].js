8516be1aa7dbd39ba87b641e4021a27e9ef29b45
Naborisk <github@naborisk.com>
2024-04-07 02:36:46 +0900

    add UTF-8 meta tag

--- functions/og/entity/[path].js
+++ functions/og/entity/[path].js
@@ -30,6 +30,7 @@ export async function onRequest(context) {
 <!DOCTYPE html>
 <html>
   <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta property="og:title" content="${username} on Concurrent">
     <meta property="og:description" content="${description}">
     <meta property="og:image" content="${avatar}">
