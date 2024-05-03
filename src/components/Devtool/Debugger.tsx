8a14e9e7e35e475b114bd62ce67809b6ed96822c
totegamma <github@gammalab.net>
2024-05-04 03:37:11 +0900

    tweak dev

--- src/components/Devtool/Debugger.tsx
+++ src/components/Devtool/Debugger.tsx
@@ -1,9 +1,45 @@
 import { Box, Button, Typography } from '@mui/material'
-import { forwardRef, useState } from 'react'
+import { forwardRef, useEffect, useState } from 'react'
 import { useClient } from '../../context/ClientContext'
 import { useSnackbar } from 'notistack'
 import { UserPicker } from '../ui/UserPicker'
 import { type User } from '@concurrent-world/client'
+import Form from '@rjsf/mui'
+import { type RegistryWidgetsType, type UiSchema, type WidgetProps } from '@rjsf/utils'
+import validator from '@rjsf/validator-ajv8'
+
+const UserPickerWidget = (props: WidgetProps): JSX.Element => {
+    const { client } = useClient()
+
+    const [selected, setSelected] = useState<User[]>([])
+    useEffect(() => {
+        if (!props.value || !client) return
+        Promise.all(props.value.map((e: string) => client.getUser(e))).then((e) => {
+            setSelected(e.filter((e) => e) as User[])
+        })
+    }, [props])
+
+    return (
+        <UserPicker
+            selected={selected}
+            setSelected={(value) => {
+                console.log(value)
+                setSelected(value)
+                props.onChange(value.map((e) => e.ccid))
+            }}
+        />
+    )
+}
+
+const uiSchema: UiSchema = {
+    users: {
+        'ui:widget': 'userPicker'
+    }
+}
+
+const widgets: RegistryWidgetsType = {
+    userPicker: UserPickerWidget
+}
 
 export const Debugger = forwardRef<HTMLDivElement>((props, ref): JSX.Element => {
     const { client } = useClient()
@@ -21,6 +57,22 @@ export const Debugger = forwardRef<HTMLDivElement>((props, ref): JSX.Element => 
                     height: '100%'
                 }}
             >
+                <Form
+                    schema={{
+                        type: 'object',
+                        properties: {
+                            users: { type: 'array', items: { type: 'string' } }
+                        }
+                    }}
+                    formData={{ users: ['con1n6t8wlevhr905fl2q72w07ehpyappvyu29cpvs'] }}
+                    uiSchema={uiSchema}
+                    widgets={widgets}
+                    validator={validator}
+                    onSubmit={(e) => {
+                        console.log(e.formData)
+                    }}
+                />
+
                 <Typography variant="h3">Debugger</Typography>
 
                 <UserPicker selected={selected} setSelected={setSelected} />
