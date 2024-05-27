349cc2072d25616180e494724e8d1999ac6868b5
totegamma <github@gammalab.net>
2024-05-27 19:46:43 +0900

    small fix

--- src/components/Settings/General.tsx
+++ src/components/Settings/General.tsx
@@ -175,7 +175,7 @@ export const GeneralSettings = (): JSX.Element => {
                                 const jwt = IssueJWT(client.keyPair.privatekey, {
                                     iss: client.ccid,
                                     aud: client.host,
-                                    sub: 'CONCURRENT_INVITE',
+                                    sub: 'CONCRNT_INVITE',
                                     exp: Math.floor((new Date().getTime() + 24 * 60 * 60 * 1000) / 1000).toString()
                                 }) // 24h validity
                                 setInvitationCode(jwt)
