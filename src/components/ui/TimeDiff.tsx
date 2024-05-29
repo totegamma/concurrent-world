586c1efb121e49af3a512a12cf5414e8dbfc63ef
totegamma <github@gammalab.net>
2024-05-29 20:41:04 +0900

    update timestamp behavior

--- src/components/ui/TimeDiff.tsx
+++ src/components/ui/TimeDiff.tsx
@@ -2,15 +2,35 @@ import { humanReadableTimeDiff } from '../../util'
 import { Box, Tooltip } from '@mui/material'
 import { useTicker } from '../../context/Ticker'
 
+import HistoryToggleOffIcon from '@mui/icons-material/HistoryToggleOff'
+
 export interface TimeDiffProps {
     date: Date
+    base?: Date
 }
+
+const threshold = 60 * 60 // 1 hour
 
 export const TimeDiff = (props: TimeDiffProps): JSX.Element => {
     useTicker()
+
+    const diff = props.base ? Math.abs(props.date.getTime() - props.base.getTime()) : 0
+    const diffTooLong = diff > threshold
+
     return (
-        <Tooltip placement="left" title={props.date.toLocaleString()}>
-            <Box component="span" width="max-content">
+        <Tooltip
+            placement="left"
+            title={diffTooLong ? 'server time: ' + props.base!.toLocaleString() : props.date.toLocaleString()}
+        >
+            <Box component="span" width="max-content" display="flex" alignItems="center" gap={0.5}>
+                {diffTooLong && (
+                    <HistoryToggleOffIcon
+                        sx={{
+                            fontSize: '0.75rem',
+                            marginLeft: '0.25rem'
+                        }}
+                    />
+                )}
                 {humanReadableTimeDiff(props.date)}
             </Box>
         </Tooltip>
