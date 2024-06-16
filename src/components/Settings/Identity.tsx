1cf53467d2edc0e83a4f6f341ca775ebab148619
totegamma <github@gammalab.net>
2024-06-16 15:15:12 +0900

    update subkey deactivate step

--- src/components/Settings/Identity.tsx
+++ src/components/Settings/Identity.tsx
@@ -12,7 +12,11 @@ import {
     Button,
     Accordion,
     AccordionSummary,
-    AccordionDetails
+    AccordionDetails,
+    Dialog,
+    DialogContent,
+    DialogTitle,
+    DialogContentText
 } from '@mui/material'
 import Tilt from 'react-parallax-tilt'
 import { Passport } from '../theming/Passport'
@@ -43,6 +47,7 @@ export const IdentitySettings = (): JSX.Element => {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
     const [hideDisabledSubKey, setHideDisabledSubKey] = usePreference('hideDisabledSubKey')
     const [aliasDraft, setAliasDraft] = useState<string>('')
+    const [deactivateTarget, setDeactivateTarget] = useState<string | null>(null)
 
     const { t } = useTranslation('', { keyPrefix: 'settings.identity' })
 
@@ -64,144 +69,7 @@ export const IdentitySettings = (): JSX.Element => {
     }
 
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 1
-            }}
-        >
-            <Box
-                sx={{
-                    padding: { xs: '10px', sm: '10px 50px' }
-                }}
-            >
-                <Tilt glareEnable={true} glareBorderRadius="5%">
-                    <Passport />
-                </Tilt>
-            </Box>
-
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={
-                        <ExpandMoreIcon
-                            sx={{
-                                color: 'text.primary'
-                            }}
-                        />
-                    }
-                >
-                    {client.user?.alias ? (
-                        <Typography variant="body1">
-                            アカウントにはエイリアス{client.user.alias}が設定されています。
-                        </Typography>
-                    ) : (
-                        <Typography variant="body1">
-                            アカウントエイリアス未設定 (マスターキーログイン時のみ設定可能)
-                        </Typography>
-                    )}
-                </AccordionSummary>
-                <AccordionDetails
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: 1
-                    }}
-                >
-                    {globalState.isMasterSession ? (
-                        <>
-                            <Typography gutterBottom>
-                                ドメインをお持ちの場合、以下のtxtレコードを作成することで自身のアカウントにエイリアスを設定できます。
-                            </Typography>
-
-                            <TextField
-                                fullWidth
-                                label="設定したいドメイン"
-                                value={aliasDraft}
-                                onChange={(e) => {
-                                    setAliasDraft(e.target.value)
-                                }}
-                            />
-
-                            <Codeblock language="js">{`対象ドメイン: _concrnt.${aliasDraft}
-レコード値:
-"ccid=${client.ccid}"
-"sig=${signature}"
-"hint=${client.host}"`}</Codeblock>
-
-                            <Button
-                                fullWidth
-                                onClick={() => {
-                                    fetch(`https://${client.host}/api/v1/entity/${aliasDraft}`)
-                                        .then(async (res) => {
-                                            const resjson = await res.json()
-                                            console.log(resjson)
-                                            if (resjson.content.alias) {
-                                                enqueueSnackbar('検証成功', { variant: 'success' })
-                                            } else {
-                                                enqueueSnackbar('検証に失敗しました。', { variant: 'error' })
-                                            }
-                                        })
-                                        .catch((e) => {
-                                            console.error(e)
-                                            enqueueSnackbar('検証に失敗しました。', { variant: 'error' })
-                                        })
-                                }}
-                            >
-                                設定を検証
-                            </Button>
-                        </>
-                    ) : (
-                        <Typography>マスターキーログイン時のみエイリアス設定が可能です。</Typography>
-                    )}
-                </AccordionDetails>
-            </Accordion>
-
-            {identity && (
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: 1
-                    }}
-                >
-                    <Alert severity="warning">{t('loginType.masterKey')}</Alert>
-
-                    <Suspense fallback={<>loading...</>}>
-                        <SwitchMasterToSub identity={identity} />
-                    </Suspense>
-                </Box>
-            )}
-
-            {subkey && (
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: 1
-                    }}
-                >
-                    <Alert severity="info">{t('loginType.subKey')}</Alert>
-                </Box>
-            )}
-
-            {!subkey && !identity && <Alert severity="error">{t('loginType.secret')}</Alert>}
-
-            <Box
-                sx={{
-                    padding: { sm: '10px 10px' }
-                }}
-            >
-                <FormGroup>
-                    <FormControlLabel
-                        control={
-                            <Switch size="small" checked={hideDisabledSubKey} onChange={toggleHideDisabledSubKey} />
-                        }
-                        label={t('hideSubKey')}
-                    />
-                </FormGroup>
-            </Box>
-
+        <>
             <Box
                 sx={{
                     display: 'flex',
@@ -209,51 +77,213 @@ export const IdentitySettings = (): JSX.Element => {
                     gap: 1
                 }}
             >
-                {keys
-                    .filter((key) => key.revokeDocument === 'null' || !hideDisabledSubKey)
-                    .map((key) => (
-                        <KeyCard
-                            key={key.id}
-                            item={key}
-                            endItem={
-                                <IconButton
-                                    sx={{
-                                        width: '40px',
-                                        height: '40px'
-                                    }}
-                                    onClick={(event) => {
-                                        setTarget(key.id)
-                                        setAnchorEl(event.currentTarget)
-                                    }}
-                                >
-                                    <MoreHorizIcon />
-                                </IconButton>
-                            }
-                        />
-                    ))}
-                <Menu
-                    anchorEl={anchorEl}
-                    open={Boolean(anchorEl)}
-                    onClose={() => {
-                        setAnchorEl(null)
+                <Box
+                    sx={{
+                        padding: { xs: '10px', sm: '10px 50px' }
                     }}
                 >
-                    <MenuItem
+                    <Tilt glareEnable={true} glareBorderRadius="5%">
+                        <Passport />
+                    </Tilt>
+                </Box>
+
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={
+                            <ExpandMoreIcon
+                                sx={{
+                                    color: 'text.primary'
+                                }}
+                            />
+                        }
+                    >
+                        {client.user?.alias ? (
+                            <Typography variant="body1">
+                                アカウントにはエイリアス{client.user.alias}が設定されています。
+                            </Typography>
+                        ) : (
+                            <Typography variant="body1">
+                                アカウントエイリアス未設定 (マスターキーログイン時のみ設定可能)
+                            </Typography>
+                        )}
+                    </AccordionSummary>
+                    <AccordionDetails
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: 1
+                        }}
+                    >
+                        {globalState.isMasterSession ? (
+                            <>
+                                <Typography gutterBottom>
+                                    ドメインをお持ちの場合、以下のtxtレコードを作成することで自身のアカウントにエイリアスを設定できます。
+                                </Typography>
+
+                                <TextField
+                                    fullWidth
+                                    label="設定したいドメイン"
+                                    value={aliasDraft}
+                                    onChange={(e) => {
+                                        setAliasDraft(e.target.value)
+                                    }}
+                                />
+
+                                <Codeblock language="js">{`対象ドメイン: _concrnt.${aliasDraft}
+レコード値:
+"ccid=${client.ccid}"
+"sig=${signature}"
+"hint=${client.host}"`}</Codeblock>
+
+                                <Button
+                                    fullWidth
+                                    onClick={() => {
+                                        fetch(`https://${client.host}/api/v1/entity/${aliasDraft}`)
+                                            .then(async (res) => {
+                                                const resjson = await res.json()
+                                                console.log(resjson)
+                                                if (resjson.content.alias) {
+                                                    enqueueSnackbar('検証成功', { variant: 'success' })
+                                                } else {
+                                                    enqueueSnackbar('検証に失敗しました。', { variant: 'error' })
+                                                }
+                                            })
+                                            .catch((e) => {
+                                                console.error(e)
+                                                enqueueSnackbar('検証に失敗しました。', { variant: 'error' })
+                                            })
+                                    }}
+                                >
+                                    設定を検証
+                                </Button>
+                            </>
+                        ) : (
+                            <Typography>マスターキーログイン時のみエイリアス設定が可能です。</Typography>
+                        )}
+                    </AccordionDetails>
+                </Accordion>
+
+                {identity && (
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: 1
+                        }}
+                    >
+                        <Alert severity="warning">{t('loginType.masterKey')}</Alert>
+
+                        <Suspense fallback={<>loading...</>}>
+                            <SwitchMasterToSub identity={identity} />
+                        </Suspense>
+                    </Box>
+                )}
+
+                {subkey && (
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: 1
+                        }}
+                    >
+                        <Alert severity="info">{t('loginType.subKey')}</Alert>
+                    </Box>
+                )}
+
+                {!subkey && !identity && <Alert severity="error">{t('loginType.secret')}</Alert>}
+
+                <Box
+                    sx={{
+                        padding: { sm: '10px 10px' }
+                    }}
+                >
+                    <FormGroup>
+                        <FormControlLabel
+                            control={
+                                <Switch size="small" checked={hideDisabledSubKey} onChange={toggleHideDisabledSubKey} />
+                            }
+                            label={t('hideSubKey')}
+                        />
+                    </FormGroup>
+                </Box>
+
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 1
+                    }}
+                >
+                    {keys
+                        .filter((key) => key.revokeDocument === 'null' || !hideDisabledSubKey)
+                        .map((key) => (
+                            <KeyCard
+                                key={key.id}
+                                item={key}
+                                endItem={
+                                    <IconButton
+                                        sx={{
+                                            width: '40px',
+                                            height: '40px'
+                                        }}
+                                        onClick={(event) => {
+                                            setTarget(key.id)
+                                            setAnchorEl(event.currentTarget)
+                                        }}
+                                    >
+                                        <MoreHorizIcon />
+                                    </IconButton>
+                                }
+                            />
+                        ))}
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={() => {
+                            setAnchorEl(null)
+                        }}
+                    >
+                        <MenuItem
+                            onClick={() => {
+                                setDeactivateTarget(target)
+                            }}
+                        >
+                            {t('deactivate')}
+                        </MenuItem>
+                    </Menu>
+                </Box>
+            </Box>
+            <Dialog
+                open={deactivateTarget !== null}
+                onClose={() => {
+                    setDeactivateTarget(null)
+                }}
+            >
+                <DialogTitle>本当に無効化しますか？</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        サブキーを無効化すると、このサブキーでログインしている端末はログアウトされます。
+                    </DialogContentText>
+                    <Button
+                        color="error"
+                        fullWidth
                         onClick={() => {
-                            if (target === null) {
+                            if (deactivateTarget === null) {
                                 return
                             }
-                            client.api.revokeSubkey(target).then(() => {
+                            client.api.revokeSubkey(deactivateTarget).then(() => {
                                 client.api.getKeyList().then((res) => {
                                     setKeys(res)
                                 })
                             })
+                            setDeactivateTarget(null)
                         }}
                     >
                         {t('deactivate')}
-                    </MenuItem>
-                </Menu>
-            </Box>
-        </Box>
+                    </Button>
+                </DialogContent>
+            </Dialog>
+        </>
     )
 }
