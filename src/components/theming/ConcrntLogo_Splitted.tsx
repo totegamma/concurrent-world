e1ed910ff9f8d98c6c07a1b1a8b806c308dbaeac
totegamma <github@gammalab.net>
2024-07-04 19:13:15 +0900

    small fix

--- src/components/theming/ConcrntLogo_Splitted.tsx
+++ src/components/theming/ConcrntLogo_Splitted.tsx
@@ -8,17 +8,17 @@ export interface ConcrntLogoSplittedProps {
 export function ConcrntLogoSplitted(props: ConcrntLogoSplittedProps): JSX.Element {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{ width: props.size, height: props.size }}>
-            <polygon
-                points="104.53 232.23 236.14 276.1 367.75 144.49 104.53 232.23"
-                style={{ fill: props.upperColor }}
-            />
-            <polygon
-                points="367.75 144.49 236.14 276.1 280.01 407.71 367.75 144.49"
-                style={{ fill: props.lowerColor }}
-            />
             <path
                 d="M422.58,256.38c0,91.95-74.8,166.75-166.75,166.75s-166.75-74.8-166.75-166.75S163.89,89.63,255.83,89.63V19.63c-63.24,0-122.69,24.63-167.41,69.34S19.09,193.14,19.09,256.38s24.63,122.69,69.34,167.41,104.17,69.34,167.41,69.34,122.69-24.63,167.41-69.34,69.34-104.17,69.34-167.41h-70Z"
                 style={{ fill: props.frameColor }}
+            />
+            <polygon
+                points="367.75 144.49 218.44 226.55 236.14 276.1 280.01 407.71 367.75 144.49"
+                style={{ fill: props.lowerColor }}
+            />
+            <polygon
+                points="104.53 232.23 236.14 276.1 367.75 144.49 104.53 232.23"
+                style={{ fill: props.upperColor }}
             />
         </svg>
     )
