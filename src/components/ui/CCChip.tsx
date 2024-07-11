07ae851f2bdc93c3ff48415628b4eb1b92e93b2f
totegamma <github@gammalab.net>
2024-07-11 22:53:06 +0900

    update stremchip

--- src/components/ui/CCChip.tsx
+++ src/components/ui/CCChip.tsx
@@ -1,21 +1,53 @@
 import { Chip, type SxProps, alpha, useTheme } from '@mui/material'
+import { type ForwardRefRenderFunction, type Ref, forwardRef } from 'react'
+import { Link as NavLink } from 'react-router-dom'
 
 export interface CCChipProps {
     label: string
     onDelete?: (event: React.MouseEvent<HTMLButtonElement>) => void
     sx?: SxProps
+    to?: string
+    size?: 'small' | 'medium'
+    icon?: JSX.Element
 }
 
-export const CCChip = (props: CCChipProps): JSX.Element => {
+const _CCChip: ForwardRefRenderFunction<HTMLDivElement | HTMLAnchorElement, CCChipProps> = (props, ref) => {
     const theme = useTheme()
+
+    if (props.to) {
+        return (
+            <Chip
+                {...props}
+                ref={ref as Ref<HTMLAnchorElement>}
+                size={props.size}
+                component={NavLink}
+                to={props.to}
+                sx={{
+                    ...props.sx,
+                    cursor: 'pointer',
+                    backgroundColor: alpha(theme.palette.text.primary, 0.1)
+                }}
+                label={props.label}
+                onDelete={props.onDelete}
+                icon={props.icon}
+            />
+        )
+    }
+
     return (
         <Chip
+            {...props}
+            ref={ref as Ref<HTMLDivElement>}
+            size={props.size}
             sx={{
                 ...props.sx,
                 backgroundColor: alpha(theme.palette.text.primary, 0.1)
             }}
             label={props.label}
             onDelete={props.onDelete}
+            icon={props.icon}
         />
     )
 }
+
+export const CCChip = forwardRef(_CCChip)
