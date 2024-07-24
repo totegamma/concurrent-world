2de9a98207f2df6c733d56e882b54e465a2d8e52
totegamma <github@gammalab.net>
2024-07-24 21:56:36 +0900

    small fix

--- functions/lib/sanitize.ts
+++ functions/lib/sanitize.ts
@@ -1,8 +1,12 @@
 export const sanitizeHtml = (unsafe: string) => {
+    try {
     return unsafe
         .replaceAll(/&/g, '&amp;')
         .replaceAll(/</g, '&lt;')
         .replaceAll(/>/g, '&gt;')
         .replaceAll(/"/g, '&quot;')
         .replaceAll(/'/g, '&#039;')
+    } catch (_) {
+        return ""
+    }
 }
