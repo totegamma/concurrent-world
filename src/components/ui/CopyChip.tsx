25ee5d78a1fc1a8274c9ce559195474fb3347c28
totegamma <github@gammalab.net>
2024-07-25 12:57:54 +0900

    tweak

--- src/components/ui/CopyChip.tsx
+++ src/components/ui/CopyChip.tsx
@@ -2,9 +2,18 @@ import { Chip, alpha, useTheme } from '@mui/material'
 import ContentPasteIcon from '@mui/icons-material/ContentPaste'
 import { useSnackbar } from 'notistack'
 
-export const CopyChip = (props: { label: string; content: string }): JSX.Element => {
+export const CopyChip = (props: { label: string; content: string; limit?: number }): JSX.Element => {
     const theme = useTheme()
     const { enqueueSnackbar } = useSnackbar()
+
+    const limit = props.limit || Infinity
+    const length = props.content.length
+    const content =
+        length > limit
+            ? `${props.content.slice(0, limit / 2)}...${props.content.slice(length - limit / 2)}`
+            : props.content
+
+    const label = `${props.label}: ${content}`
 
     return (
         <Chip
@@ -13,7 +22,7 @@ export const CopyChip = (props: { label: string; content: string }): JSX.Element
                 backgroundColor: alpha(theme.palette.text.primary, 0.1)
             }}
             size="small"
-            label={props.label}
+            label={label}
             deleteIcon={<ContentPasteIcon />}
             onDelete={() => {
                 navigator.clipboard.writeText(props.content)
