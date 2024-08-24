47e6cf2e90a53f6cce1a0d0db62aa9bae46d15f0
totegamma <github@gammalab.net>
2024-08-25 02:28:40 +0900

    add notification filter. fix #578

---
+++ src/components/QueryTimeline.tsx
@@ -0,0 +1,328 @@
+import { Box, Divider, ListItem, ListItemIcon, ListItemText, type SxProps, Typography, useTheme } from '@mui/material'
+import React, { memo, useCallback, useEffect, useState, useRef, forwardRef, type ForwardedRef } from 'react'
+import { AssociationFrame } from './Association/AssociationFrame'
+import { Loading } from './ui/Loading'
+import { MessageContainer } from './Message/MessageContainer'
+import { ErrorBoundary, type FallbackProps } from 'react-error-boundary'
+import HeartBrokenIcon from '@mui/icons-material/HeartBroken'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
+import SyncIcon from '@mui/icons-material/Sync'
+import { type Query, type QueryTimelineReader as CoreTimelineReader } from '@concurrent-world/client'
+import useSound from 'use-sound'
+import { usePreference } from '../context/PreferenceContext'
+import { VList, type VListHandle } from 'virtua'
+import { useClient } from '../context/ClientContext'
+import { UseSoundFormats } from '../constants'
+
+export interface TimelineProps {
+    timeline: string
+    query: Query
+    batchSize?: number
+    perspective?: string
+    header?: JSX.Element
+    onScroll?: (top: number) => void
+}
+
+const PTR_HEIGHT = 60
+
+const divider = (
+    <Divider
+        variant="inset"
+        component="li"
+        sx={{
+            mx: { xs: 0.5, sm: 1, md: 1 }
+        }}
+    />
+)
+const timelineElemSx: SxProps = {
+    p: { xs: 0.5, sm: 1, md: 1 }
+}
+
+const timeline = forwardRef((props: TimelineProps, ref: ForwardedRef<VListHandle>): JSX.Element => {
+    const { client } = useClient()
+    const theme = useTheme()
+    const [sound] = usePreference('sound')
+
+    const timeline = useRef<CoreTimelineReader | null>(null)
+
+    const [hasMoreData, setHasMoreData] = useState<boolean>(false)
+    const [isFetching, setIsFetching] = useState<boolean>(false)
+
+    const [touchPosition, setTouchPosition] = useState<number>(0)
+    const [loaderSize, setLoaderSize] = useState<number>(0)
+    const [loadable, setLoadable] = useState<boolean>(false)
+    const [ptrEnabled, setPtrEnabled] = useState<boolean>(false)
+
+    const [playBubble] = useSound(sound.post, {
+        volume: sound.volume / 100,
+        interrupt: false,
+        format: UseSoundFormats
+    })
+    const playBubbleRef = useRef(playBubble)
+
+    const [timelineLoading, setTimelineLoading] = useState<boolean>(true)
+
+    useEffect(() => {
+        playBubbleRef.current = playBubble
+    }, [playBubble])
+
+    useEffect(() => {
+        let isCancelled = false
+        setTimelineLoading(true)
+        client.newTimelineQuery().then((t) => {
+            if (isCancelled) return
+            timeline.current = t
+            timeline.current
+                .init(props.timeline, props.query, props.batchSize ?? 16)
+                .then((hasMore) => {
+                    setHasMoreData(hasMore)
+                })
+                .finally(() => {
+                    setTimelineLoading(false)
+                })
+            return t
+        })
+        return () => {
+            isCancelled = true
+        }
+    }, [props.timeline, props.query, client])
+
+    const positionRef = useRef<number>(0)
+    const scrollParentRef = useRef<HTMLDivElement>(null)
+
+    const onTouchStart = useCallback((raw: Event) => {
+        const e = raw as TouchEvent
+        setTouchPosition(e.touches[0].clientY)
+        setLoadable(positionRef.current === 0)
+    }, [])
+
+    const onTouchMove = useCallback(
+        (raw: Event) => {
+            if (!loadable) return
+            const e = raw as TouchEvent
+            if (!scrollParentRef.current) return
+            const delta = e.touches[0].clientY - touchPosition
+            setLoaderSize(Math.min(Math.max(delta, 0), PTR_HEIGHT))
+            if (delta >= PTR_HEIGHT) setPtrEnabled(true)
+        },
+        [scrollParentRef.current, touchPosition]
+    )
+
+    const onTouchEnd = useCallback(() => {
+        setLoaderSize(0)
+        if (ptrEnabled) {
+            if (isFetching) {
+                setPtrEnabled(false)
+                return
+            }
+            setIsFetching(true)
+            setHasMoreData(false)
+            setTimeout(() => {
+                timeline.current?.reload().then((hasMore) => {
+                    setHasMoreData(hasMore)
+                    setIsFetching(false)
+                    setPtrEnabled(false)
+                })
+            }, 1000)
+        }
+    }, [ptrEnabled, setPtrEnabled, props.timeline, props.query, client.api, isFetching])
+
+    useEffect(() => {
+        if (!scrollParentRef.current) return
+        scrollParentRef.current.addEventListener('touchstart', onTouchStart)
+        scrollParentRef.current.addEventListener('touchmove', onTouchMove)
+        scrollParentRef.current.addEventListener('touchend', onTouchEnd)
+        return () => {
+            scrollParentRef.current?.removeEventListener('touchstart', onTouchStart)
+            scrollParentRef.current?.removeEventListener('touchmove', onTouchMove)
+            scrollParentRef.current?.removeEventListener('touchend', onTouchEnd)
+        }
+    }, [scrollParentRef.current, onTouchStart, onTouchMove, onTouchEnd])
+
+    const count = timeline.current?.body.length ?? 0
+    let alreadyFetchInThisRender = false
+
+    const readMore = (): void => {
+        if (isFetching || alreadyFetchInThisRender) return
+        setIsFetching(true)
+        alreadyFetchInThisRender = true
+
+        console.log('readMore!!')
+        timeline.current
+            ?.readMore()
+            .then((hasMore) => {
+                setHasMoreData(hasMore)
+                alreadyFetchInThisRender = false
+            })
+            .finally(() => {
+                setIsFetching(false)
+                alreadyFetchInThisRender = false
+            })
+    }
+
+    return (
+        <>
+            <Box
+                sx={{
+                    height: `${ptrEnabled ? PTR_HEIGHT : loaderSize}px`,
+                    width: '100%',
+                    position: 'relative',
+                    color: 'text.secondary',
+                    display: 'flex',
+                    transition: 'height 0.2s ease-in-out',
+                    overflow: 'hidden'
+                }}
+            >
+                <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    height={`${PTR_HEIGHT}px`}
+                    position="absolute"
+                    width="100%"
+                    bottom="0"
+                    left="0"
+                >
+                    {isFetching ? (
+                        <SyncIcon
+                            sx={{
+                                animation: 'spin 1s linear infinite',
+                                '@keyframes spin': {
+                                    '0%': { transform: 'rotate(0deg)' },
+                                    '100%': { transform: 'rotate(-360deg)' }
+                                }
+                            }}
+                        />
+                    ) : (
+                        <ArrowUpwardIcon
+                            sx={{
+                                transform: `rotate(${ptrEnabled ? 0 : 180}deg)`,
+                                transition: 'transform 0.2s ease-in-out'
+                            }}
+                        />
+                    )}
+                </Box>
+            </Box>
+            <Box
+                ref={scrollParentRef}
+                sx={{
+                    display: 'flex',
+                    flex: 1,
+                    overflow: 'hidden',
+                    flexDirection: 'column'
+                }}
+            >
+                <VList
+                    style={{
+                        flex: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        listStyle: 'none',
+                        overflowX: 'hidden',
+                        overflowY: 'auto',
+                        overscrollBehaviorY: 'none',
+                        scrollbarGutter: 'stable'
+                    }}
+                    onScroll={(top) => {
+                        positionRef.current = top
+                        props.onScroll?.(top)
+                    }}
+                    onRangeChange={(_, end) => {
+                        if (end + 3 > count && hasMoreData) readMore()
+                    }}
+                    ref={ref}
+                >
+                    {props.header}
+
+                    {(timeline.current?.body.length ?? 0) === 0
+                        ? !timelineLoading && (
+                              <Box
+                                  sx={{
+                                      width: '100%',
+                                      display: 'flex',
+                                      justifyContent: 'center'
+                                  }}
+                              >
+                                  <Typography
+                                      sx={{
+                                          color: 'text.secondary'
+                                      }}
+                                  >
+                                      No currents yet here!
+                                  </Typography>
+                              </Box>
+                          )
+                        : (timelineLoading ? [] : timeline.current?.body ?? []).map((e) => {
+                              let element
+                              const type = e.resourceID[0]
+                              switch (type) {
+                                  case 'm':
+                                      element = (
+                                          <MessageContainer
+                                              sx={timelineElemSx}
+                                              messageID={e.resourceID}
+                                              messageOwner={e.owner}
+                                              resolveHint={e.timelineID.split('@')[1]}
+                                              lastUpdated={e.lastUpdate?.getTime() ?? 0}
+                                              after={divider}
+                                              timestamp={e.cdate}
+                                          />
+                                      )
+                                      break
+                                  case 'a':
+                                      element = (
+                                          <AssociationFrame
+                                              sx={timelineElemSx}
+                                              associationID={e.resourceID}
+                                              associationOwner={e.owner}
+                                              lastUpdated={e.lastUpdate?.getTime() ?? 0}
+                                              after={divider}
+                                              perspective={props.perspective}
+                                          />
+                                      )
+                                      break
+                                  default:
+                                      element = <Typography>Unknown message type: {type}</Typography>
+                                      break
+                              }
+
+                              return (
+                                  <React.Fragment key={e.resourceID}>
+                                      <ErrorBoundary FallbackComponent={renderError}>{element}</ErrorBoundary>
+                                  </React.Fragment>
+                              )
+                          })}
+
+                    {!timelineLoading && isFetching && (
+                        <Loading key={0} message="Loading..." color={theme.palette.text.primary} />
+                    )}
+                </VList>
+            </Box>
+        </>
+    )
+})
+timeline.displayName = 'timeline'
+
+const renderError = ({ error }: FallbackProps): JSX.Element => {
+    const [isDevMode] = usePreference('devMode')
+    if (!isDevMode) return <></>
+    return (
+        <ListItem>
+            <ListItemIcon>
+                <HeartBrokenIcon />
+            </ListItemIcon>
+            <ListItemText
+                primary="この要素の描画中に問題が発生しました"
+                secondary={
+                    <Box>
+                        {error?.message}
+                        <pre>{error?.stack}</pre>
+                    </Box>
+                }
+            />
+        </ListItem>
+    )
+}
+
+export const QueryTimelineReader = memo(timeline)
+QueryTimelineReader.displayName = 'QueryTimelineReader'
--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -1,17 +1,17 @@
-import { Box, Divider, Typography } from '@mui/material'
-import { useMemo } from 'react'
-import { Timeline } from '../components/Timeline'
+import { Box, Chip, Divider, Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { useClient } from '../context/ClientContext'
+import { QueryTimelineReader } from '../components/QueryTimeline'
+import { useState } from 'react'
+import { Schemas } from '@concurrent-world/client'
 
 export function Notifications(): JSX.Element {
     const { t } = useTranslation('', { keyPrefix: 'pages.notifications' })
     const { client } = useClient()
 
-    const streams = useMemo(() => {
-        const target = client.user?.notificationTimeline
-        return target ? [target] : []
-    }, [client])
+    const timeline = client.user?.notificationTimeline
+
+    const [selected, setSelected] = useState<string | undefined>(undefined)
 
     return (
         <Box
@@ -31,8 +31,70 @@ export function Notifications(): JSX.Element {
             >
                 <Typography variant="h2">{t('title')}</Typography>
                 <Divider />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        gap: 1,
+                        py: 1
+                    }}
+                >
+                    <Chip
+                        label={'リプライ'}
+                        onClick={() => {
+                            setSelected(selected === Schemas.replyAssociation ? undefined : Schemas.replyAssociation)
+                        }}
+                        color="primary"
+                        variant={selected === Schemas.replyAssociation ? 'filled' : 'outlined'}
+                    />
+                    <Chip
+                        label={'メンション'}
+                        onClick={() => {
+                            setSelected(
+                                selected === Schemas.mentionAssociation ? undefined : Schemas.mentionAssociation
+                            )
+                        }}
+                        color="primary"
+                        variant={selected === Schemas.mentionAssociation ? 'filled' : 'outlined'}
+                    />
+                    <Chip
+                        label={'リルート'}
+                        onClick={() => {
+                            setSelected(
+                                selected === Schemas.rerouteAssociation ? undefined : Schemas.rerouteAssociation
+                            )
+                        }}
+                        color="primary"
+                        variant={selected === Schemas.rerouteAssociation ? 'filled' : 'outlined'}
+                    />
+                    <Chip
+                        label={'お気に入り'}
+                        onClick={() => {
+                            setSelected(selected === Schemas.likeAssociation ? undefined : Schemas.likeAssociation)
+                        }}
+                        color="primary"
+                        variant={selected === Schemas.likeAssociation ? 'filled' : 'outlined'}
+                    />
+                    <Chip
+                        label={'リアクション'}
+                        onClick={() => {
+                            setSelected(
+                                selected === Schemas.reactionAssociation ? undefined : Schemas.reactionAssociation
+                            )
+                        }}
+                        color="primary"
+                        variant={selected === Schemas.reactionAssociation ? 'filled' : 'outlined'}
+                    />
+                </Box>
+                <Divider />
             </Box>
-            <Timeline streams={streams} />
+            {timeline && (
+                <QueryTimelineReader
+                    timeline={timeline}
+                    query={{
+                        schema: selected
+                    }}
+                />
+            )}
         </Box>
     )
 }
