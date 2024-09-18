59a28d21b7b479231521d5b93bbab235a9325605
totegamma <github@gammalab.net>
2024-09-18 22:58:58 +0900

    update settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        sourcemap: true,
+    },
     server: {
       host: '0.0.0.0',
     },
